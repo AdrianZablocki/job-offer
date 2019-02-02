424bd7e650a95199f42c9761bbe2b3423f5ddfd1
Adrian Zablocki <adrian.zablocki@icloud.com>
2019-02-02 13:00:17 +0100

    Add basic offers slider

--- src/components/advert-slider/advert-slider.ts
+++ src/components/advert-slider/advert-slider.ts
@@ -7,7 +7,20 @@ export class AdvertSlider {
 
     public render(): string {
         return `
-
+            <div>
+                <header>
+                    <h1>${this.advert.position}</h1>
+                    <div>hamburger</div>
+                </header>
+                <div>
+                    <div>${this.advert.company}</div>
+                    <div>${this.advert.location}</div>
+                </div>
+                <div>
+                    <span id="arrowLeft">arrow left</span>
+                    <span id="arrowRight">arrow right</span>
+                </div>
+            </div>
         `;
     }
 }
